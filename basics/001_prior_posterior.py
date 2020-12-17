5fe961237909e96f43d328163521bcf282446045
Eshan M. Herath <eshan.m.herath@gmail.com>
2020-12-17 09:59:49 +0530

    Feat: single measurement update

---
+++ basics/001_prior_posterior.py
@@ -0,0 +1,37 @@
+# Assume a 1D world with 5 states
+# Robot doesnt know where it is.
+
+# Prior Probabilities (From Normal Distribution)
+p = [0.2, 0.2, 0.2, 0.2, 0.2]
+
+# World State
+world = ['green', 'red', 'red', 'green', 'green']
+
+# Now Robot sees that its in a green cell (Measurement
+Z = 'green'
+
+# We define these
+p_hit = 0.6
+p_miss = 0.2
+
+
+def sense(p, Z):
+    q = []
+    for p1, w1 in zip(p, world):
+        if w1 == Z:
+            q.append(p1 * p_hit)
+        else:
+            q.append(p1 * p_miss)
+
+    # To make q a proper probability distribution, we normalize it
+    summation = sum(q)
+
+    q = [i / summation for i in q]
+
+    return q
+
+
+# We get the posterior distribution (given the fact that Robot has observed a measurement)
+
+posterior = sense(p, Z)
+print(posterior)
